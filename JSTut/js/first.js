de67d594444f8480fab29a54fc0c52825d40a30c
Harsh8279 <hhenu8279@gmail.com>
2022-01-09 14:20:59 +0530

    Statement and comment in JS

--- JSTut/js/first.js
+++
@@ -1,7 +0,0 @@
-console.log("Harsh Patel");
-
-// See output in console
-
-alert(5+6);
-
-// Use alert to see the pop up in window
---
+++ JSTut/js/script.js
@@ -0,0 +1,14 @@
+var a;
+a = 5;
+alert(a);
+a
+=
+4;
+// we can write the statement like this bcz it seperates with ;
+alert(a);
+
+/*
+
+    Hii This is Multiline comment
+
+*/
