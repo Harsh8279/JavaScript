3db257ccca12fcd933f38b15c2b5353d1648c1ae
Harsh8279 <hhenu8279@gmail.com>
2022-01-16 04:54:06 +0530

    logical operator

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -22,3 +22,31 @@ console.log(a===b);
 
 // in == only value comparison 
 // while in === value + datatype comparison
+
+a = 5;
+b = "5";
+
+console.log("a!=b ",a!=b);
+
+console.log("a!==b",a!==b);
+
+a = 5;
+b = 6;
+
+console.log("a ==>",a,"\nb ==>",b);
+
+console.log("a>b ==>",a>b);
+
+console.log("a<b ==>",a<b);
+
+// Logical Operator
+
+console.log("5>4 && 4<5 ==>",5>4 && 4<5);
+console.log("5>6 && 4<5 ==>",5>6 && 4<5);
+console.log("5>6 || 4<5 ==>",5>6 || 4<5);
+
+console.log("6==5 ==>",6==5);
+console.log("!(6==5) ==>",!(6==5))
+
+
+
