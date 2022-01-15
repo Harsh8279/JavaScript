349631f25ef6a1f70bfcfd845a88e44e327f0c66
Harsh8279 <hhenu8279@gmail.com>
2022-01-16 05:10:04 +0530

    Basic understanding of If Else

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,52 +1,16 @@
-// comparison operator 
+let age = 18;
+
+// let hasVoterIdCard = true;
+let hasVoterIdCard = false;
 
 
-// ==  & ===
-
-let a = 3;
-let b = 4;
-
-console.log(a==b);
-
-console.log(a===b);
-
-
-a = 4;
-console.log(a==b);
-console.log(a===b);
-
-b = "4";
-
-console.log(a==b);
-console.log(a===b);
-
-// in == only value comparison 
-// while in === value + datatype comparison
-
-a = 5;
-b = "5";
-
-console.log("a!=b ",a!=b);
-
-console.log("a!==b",a!==b);
-
-a = 5;
-b = 6;
-
-console.log("a ==>",a,"\nb ==>",b);
-
-console.log("a>b ==>",a>b);
-
-console.log("a<b ==>",a<b);
-
-// Logical Operator
-
-console.log("5>4 && 4<5 ==>",5>4 && 4<5);
-console.log("5>6 && 4<5 ==>",5>6 && 4<5);
-console.log("5>6 || 4<5 ==>",5>6 || 4<5);
-
-console.log("6==5 ==>",6==5);
-console.log("!(6==5) ==>",!(6==5))
-
-
-
+if(age>=18 && hasVoterIdCard){
+    alert("You can vote");
+}
+else if(age>=18 && !hasVoterIdCard)
+{
+    alert("Please visit for making voter id card!!!");
+}
+else{
+    alert("Sorry!! Not Eligible!!");
+}
