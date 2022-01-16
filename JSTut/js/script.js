f14e2cd266d61dfad1eff8e080990d7a2de3df6a
Harsh8279 <hhenu8279@gmail.com>
2022-01-16 09:09:15 +0530

    Break, Continue and Nested for loops

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,14 +1,36 @@
-let cnt = 0;
+// break , continue
+document.write("break<br>");
+for(let cnt = 1;cnt<10;cnt++){
+    if (cnt==5){
+        break;
+    }
 
-while(cnt<=10){
-    document.write("Hi Harsh!!<br>");
-    cnt++;
+    document.write(cnt);
+    document.write("<br>");
 }
-document.write(cnt);
-do{
-    document.write("<br>Good Morning!!");
-}while(cnt<10);
-document.write("<br>");
-for(let i=0;i<10;i++){
-    document.write("Hi Swayam!!");
+document.write("Continue<br>");
+for(let cnt = 1;cnt<10;cnt++){
+    if (cnt==5){
+        continue;
+    }
+
+    document.write(cnt);
+    document.write("<br>");
+}
+document.write("Nested For Loop<br>");
+for(let i=1;i<10;i++){
+    document.write(i);
+    document.write("<br>");
+
+    for(let j=1;j<3;j++){
+        
+        if(i==3){
+            // break; 
+            continue;
+        }
+
+        document.write("Harsh");
+    }
+    document.write("<br>");
+
 }
