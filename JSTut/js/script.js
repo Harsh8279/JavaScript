d06180fdac09bb69d48a5bfbc6e102970ca8d4a0
Harsh8279 <hhenu8279@gmail.com>
2022-01-16 06:00:01 +0530

    Switch case

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,9 +1,75 @@
-// let user = "Harsh";
-let user;
+// input = 1 , "y", "yes"   ==> print continue...
+// input = 0, "n", "no"     ==> print end..
 
-alert(user);
+let input = 1;
 
-// while we have value of variable as undefined then we can use 
-// Nullish coalescing operator --> ??
+// input = 'n';
 
-alert(user ?? "Guest User");
+// input = '';
+
+input = 'yes';
+
+/*
+if(input===1){
+
+    document.write("Continue...");
+
+}else if(input==='y'){
+    document.write("Continue...");
+}
+else if(input==='yes'){
+    document.write("Continue...");
+}
+else if(input===0){
+    document.write("End...");
+}
+else if(input==='n'){
+    document.write("End...");
+}
+else if(input==='no'){
+    document.write("End...");
+}
+else{
+    document.write("Invalid !!!");
+}
+
+*/
+
+// Switch statement 
+
+// Switch only use --> === (Strict comparison)
+
+input = 'y';
+
+switch(input){
+
+    case 1 :    // if(input===1)
+        document.write("Continue...");
+    break;
+
+    case 'y':   // if(input==='y')
+        document.write("Continue...");
+    break;
+
+    case 'yes': // if(input==='yes')
+        document.write("Continue...");
+    break;
+
+    case 0:     // if(input===0)
+        document.write("End...");
+    break;
+
+    case 'n':   // if(input==='n')
+        document.write("End...");
+    break;
+
+    case 'no':  // if(input==='no')
+        document.write("End...");
+    break;
+
+    default:
+        document.write("Invalid !!!");
+
+    break;
+}
+
