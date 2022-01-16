4bc7a68c796b715c528558101afd760e2f9096fe
Harsh8279 <hhenu8279@gmail.com>
2022-01-16 08:41:14 +0530

    Loop concept

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,75 +1,14 @@
-// input = 1 , "y", "yes"   ==> print continue...
-// input = 0, "n", "no"     ==> print end..
+let cnt = 0;
 
-let input = 1;
-
-// input = 'n';
-
-// input = '';
-
-input = 'yes';
-
-/*
-if(input===1){
-
-    document.write("Continue...");
-
-}else if(input==='y'){
-    document.write("Continue...");
+while(cnt<=10){
+    document.write("Hi Harsh!!<br>");
+    cnt++;
 }
-else if(input==='yes'){
-    document.write("Continue...");
+document.write(cnt);
+do{
+    document.write("<br>Good Morning!!");
+}while(cnt<10);
+document.write("<br>");
+for(let i=0;i<10;i++){
+    document.write("Hi Swayam!!");
 }
-else if(input===0){
-    document.write("End...");
-}
-else if(input==='n'){
-    document.write("End...");
-}
-else if(input==='no'){
-    document.write("End...");
-}
-else{
-    document.write("Invalid !!!");
-}
-
-*/
-
-// Switch statement 
-
-// Switch only use --> === (Strict comparison)
-
-input = 'y';
-
-switch(input){
-
-    case 1 :    // if(input===1)
-        document.write("Continue...");
-    break;
-
-    case 'y':   // if(input==='y')
-        document.write("Continue...");
-    break;
-
-    case 'yes': // if(input==='yes')
-        document.write("Continue...");
-    break;
-
-    case 0:     // if(input===0)
-        document.write("End...");
-    break;
-
-    case 'n':   // if(input==='n')
-        document.write("End...");
-    break;
-
-    case 'no':  // if(input==='no')
-        document.write("End...");
-    break;
-
-    default:
-        document.write("Invalid !!!");
-
-    break;
-}
-
