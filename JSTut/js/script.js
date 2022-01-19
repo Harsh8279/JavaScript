457503b6f1b4b8a5b1ad03720b34161a4da30ee0
Harsh8279 <hhenu8279@gmail.com>
2022-01-20 01:30:09 +0530

    alert, prompt and confirm

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,18 +1,45 @@
-// label to for loops 
+/*
 
-outer:for(let i =1;i<=10;i++){ //   labelizing the loop
-    
-    document.write(i);
-    document.write("<br>");
-    for(let j=1;j<3;j++){
-        
-        // now I want to break outer loop using 
-        // this inner loop condition
-        if(i==5){
-            break outer;
-        }
-        document.write("Inner Loop");
-        document.write("<br>");
-    }
+    1. alert() --> shows a message 
 
+    2. prompt() --> shows a message, input text. It returns text on Ok or, 
+                    if Cancel button or Esc is clicked, null.
+
+    3. confirm() --> shows a message, confirm with "ok" or "cancel". It 
+                     returns true for Ok and false for cancel\Esc.
+
+*/
+
+// Note :- All these pause script execution and don't allow  the visitor 
+// to interact with the rest of the page until the window has been dismissed.
+
+alert("Welcome !!");
+
+// let age = prompt("Enter Your Age");
+
+// If we want by default value in text field then 
+
+let age = prompt("Enter Your age ",21);
+
+// document.write("Age is ",age, " type is ",typeof age);
+
+// document.write(age ?? 'Age is blank');
+
+if(age != null){
+    document.write("Age is ",age, " type is ",typeof age);
+}
+else{
+    document.write("Age is not given by user!!");
+}
+
+// confirm
+
+let response = confirm("Do you want to Quit??");
+
+if(response){
+
+    document.write("<br>Bbbyee!!");
+
+}else{
+    document.write("<br>Welcome Back!!");
 }
