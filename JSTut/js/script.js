caafc81cc87ba026a71ed0732eddb879092db365
Harsh8279 <hhenu8279@gmail.com>
2022-01-20 02:05:11 +0530

    Type conversion in JS

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,38 +1,101 @@
-// data type conversion
+/**
+ 
+    Number()
+    String()
+    Boolean()
 
-let a = 5;
+    + 
 
-console.log("a ",a, " type of",typeof a);
-
-a = a + null;
-
-console.log("a",a," type of",typeof a);
+ */
 
 
-a = "5"+null;
+let a  = 4;
 
-console.log("a",a," type of",typeof a);
+console.log("a -->",a," typeof --> ",typeof a);
 
-a = "5" + 2;
+a = String(a);
 
-console.log("a",a," type of",typeof a);
+console.log("a -->",a," typeof --> ",typeof a);
 
-a = 2 + "5";
+a = Boolean(a);
 
-console.log("a",a," type of",typeof a);
+console.log("a -->",a," typeof --> ",typeof a);
 
-a = 2 - "5";
+a = 5;
 
-console.log("a",a," type of",typeof a);
+console.log("a -->",a," typeof --> ",typeof a);
 
-a = 2 * "5";
+a = Boolean(a);
 
-console.log("a",a," type of",typeof a);
+console.log("a -->",a," typeof --> ",typeof a);
 
-a = "5"*"2";
+a = 0;
 
-console.log("a",a," type of",typeof a);
+console.log("a -->",a," typeof --> ",typeof a);
 
-a = "5"*"yes";
+a = Boolean(a);
 
-console.log("a",a," type of",typeof a);
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = "";
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = Boolean(a);
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = "a";
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = Boolean(a);
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = "a";
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = Number(a);
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = "1";
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = Number(a);
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = "3";
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = + a;
+console.log("a -->",a," typeof --> ",typeof a);
+
+a ="abcd";
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a  = + a;
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = true;
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a  = Number(a);
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = false;
+
+console.log("a -->",a," typeof --> ",typeof a);
+
+a = Number(a);
+
+console.log("a -->",a," typeof --> ",typeof a);
