4fc0bcb2e17cba41da1f1d8a24d0e574a7332231
Harsh8279 <hhenu8279@gmail.com>
2022-01-20 13:31:10 +0530

    Basics of Array

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,163 +1,30 @@
-let pName  = "Swayam";
+// Array 
 
-// let sntc = `Hi ${name} Good Morning!!`;     // In a browser, the global name variable has special meaning. 
+let arr1 = new Array("A","B","C","D");
 
-let sntc = `Hi ${pName} Good Morning!!`;
+console.log(arr1);
 
-console.log(sntc);
+arr1 = ["a","b","c","d","e","f"];
 
-sntc  = `Hi ${pName}\nGood \tMorning!!`;
+console.log(arr1);
 
-console.log(sntc);
+console.log("arr1[4]",arr1[4]);
 
-sntc = "Hi\Harsh How are you???";
+console.log(typeof arr1);
 
-console.log(sntc);
+arr1 = [1,2,3,4.5,23,"Hi"];
 
-sntc = "Hi\\Harsh How are you???";
+console.log(arr1);
 
-console.log(sntc);
+console.log(typeof arr1);
 
-// sntc = "Hi Harsh"Good Morning"; --> Invaid
+console.log(arr1);
 
-sntc = "Hi \"Harsh How are you???";
+arr1[1] = "Harsh";
 
-console.log(sntc);
-
-sntc =  'That book is Harsh\'s book ri8??';
-
-console.log(sntc);
-
-sntc = "Hi\Harsh";
-
-console.log(sntc.length);
-
-sntc = "Hi\\Harsh";
-console.log(sntc);
-console.log(sntc.length);
-
-sntc = "Swayam Patel";
-
-console.log("sntc ",sntc);
-
-console.log("sntc[4]",sntc[4],"sntc[5]",sntc[5]);
-
-// case sensitive
-sntc = "harsh";
-
-if("Harsh"==sntc){
-    console.log("Equal");
-}
-else{
-    console.log("Not Equal");
-}
-
-let str1 = "Hi";
-
-let str2 = "Swayam";
-
-let str3 = str1.concat(str2);
-
-console.log(str3);
-
-str3 = str1.concat(" ",str2);
-
-console.log(str3);
-
-sntc = "Hi Swayam Patel Good Morning!!";
-
-let subSntc = sntc.substring(3,15);
-
-console.log(sntc);
-console.log("sntc.substring(3,15) ",subSntc);
-
-let position = sntc.indexOf("Good");
-
-console.log(sntc);
-
-console.log("sntc.indexOf(\"Good\")", position);
-
-position = sntc.indexOf("good");
-
-console.log(sntc);
-
-console.log("sntc.indexOf(\"good\")", position);
-
-position = sntc.indexOf("Good",17); // we can give from which point we want search
-
-console.log(sntc);
-
-console.log("sntc.indexOf(\"Good\",17)", position);
-
-position = sntc.indexOf("Good",2); // we can give from which point we want search
-
-console.log(sntc);
-
-console.log("sntc.indexOf(\"Good\",2)", position);
-
-sntc = "Hi Harsh, How are you? All are good or not?"
-
-position = sntc.indexOf("are");
-
-let lstPosition = sntc.lastIndexOf("are");
-
-console.log(sntc);
-
-console.log("sntc.indexOf(\"are\")",position);
-
-console.log("sntc.lastIndexOf(\"are\")", lstPosition);
-
-sntc = "     Hi Harsh how are you? Where are yoou going now???      ";
-
-let trimmedSntc = sntc.trim();
-console.log("=============================================================");
-console.log(sntc)
-console.log(trimmedSntc);
-console.log("=============================================================");
-trimmedSntc = sntc.trimEnd();
-console.log("=============================================================");
-console.log(sntc)
-console.log(trimmedSntc);
-console.log("=============================================================");
-
-sntc = "Hi Harsh Hw Are you I am FINE NA? ";
-
-let uperSntc = sntc.toUpperCase();
-console.log("=============================================================");
-console.log(sntc)
-console.log(uperSntc);
-console.log("=============================================================");
-
-let lwrSntc = sntc.toLowerCase();
-console.log("=============================================================");
-console.log(sntc)
-console.log(lwrSntc);
-console.log("=============================================================");
-
-let lcllwrSntc = sntc.toLocaleLowerCase();
-console.log("=============================================================");
-console.log(sntc)
-console.log(lcllwrSntc);
-console.log("=============================================================");
-
-let lcluprSntc = sntc.toLocaleUpperCase();
-console.log("=============================================================");
-console.log(sntc)
-console.log(lcluprSntc);
-console.log("=============================================================");
-
-sntc = "Hi Harsh Patel How are You???";
-
-let rplcsntc = sntc.replace("Harsh","Swayam");
-console.log("=============================================================");
-console.log(sntc)
-console.log(rplcsntc);
-console.log("=============================================================");
-
-let incValue  = sntc.includes("Harsh");
-
-console.log("=============================================================");
-console.log(sntc)
-console.log("sntc.includes(\"Harsh\")",incValue);
-console.log("sntc.includes(\"harsh\")",sntc.includes("harsh"));
-console.log("=============================================================");
+console.log(arr1);
+/*
+    An element inside an array can be of any type, 
+    and different elements of the same array can be of different types
+    : string, boolean, even objects or other arrays.
+*/
