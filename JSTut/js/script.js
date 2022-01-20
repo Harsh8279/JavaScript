235db809762564f4cd9e5f483450efd77fdb1a50
Harsh8279 <hhenu8279@gmail.com>
2022-01-20 17:45:07 +0530

    Array Operations

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,30 +1,166 @@
-// Array 
+// Operations on Array
 
-let arr1 = new Array("A","B","C","D");
+let books = ["Maths","Physics","Chemistry","C++","Java","JavaScript"];
 
-console.log(arr1);
+console.log(books);
 
-arr1 = ["a","b","c","d","e","f"];
+console.log("\tLength of the array is ",books.length);
 
-console.log(arr1);
+books.push("Python");       // Use of push method
 
-console.log("arr1[4]",arr1[4]);
+console.log(books);
 
-console.log(typeof arr1);
+console.log("\tLength of the array is ",books.length);
 
-arr1 = [1,2,3,4.5,23,"Hi"];
+books.unshift("Harsh");// use of unshift method --> Elements to insert at the start of the array.
 
-console.log(arr1);
+console.log(books);
 
-console.log(typeof arr1);
+console.log("\tLength of the array is ",books.length);
 
-console.log(arr1);
+books.pop()     // use of pop --> Removes the last element from an array and returns it. 
+                // If the array is empty, undefined is returned and the array is not modified.
 
-arr1[1] = "Harsh";
+console.log(books);
 
-console.log(arr1);
+console.log("\tLength of the array is ",books.length);
+
+books.pop();
+
+books.pop();
+
+
+console.log(books);
+
+console.log("\tLength of the array is ",books.length);
+
+books.shift();   // use of shift --> Removes the first element from an array and returns it. 
+                // If the array is empty, undefined is returned and the array is not modified.
+
+console.log(books);
+
+console.log("\tLength of the array is ",books.length);
+
+books = ["Hi","Harsh",1,2,3,4,5,6,7,8,9,"bbbye"];
+
+console.log(books);
+
+books.splice(1,7);     
+
 /*
-    An element inside an array can be of any type, 
-    and different elements of the same array can be of different types
-    : string, boolean, even objects or other arrays.
+
+    Use of splice()
+
+The zero-based location in the array from which to start removing elements.
+Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+@returns — An array containing the elements that were deleted.
+
 */
+
+console.log(books);
+
+console.log("\tLength of the array is ",books.length);
+
+books = ["Hi","Harsh",1,2,3,4,5,6,7,8,9,"bbbye"];
+
+console.log(books);
+
+books.splice(7,4);  // i.e. -> from position 7th delete next 4-1 elements 
+
+console.log(books);
+
+console.log("\tLength of the array is ",books.length);
+
+books.length =0;
+
+console.log(books);
+
+console.log("\tLength of the array is ",books.length);
+
+books = ["Hi","Harsh",1,2,3,4,5,6,7,8,9,"bbbye"];
+
+console.log(books);
+
+let position = books.indexOf(1);
+
+console.log(position);
+// isArray --> used to check whether given variable is Array or not.
+let arr = "Hi";
+
+console.log(Array.isArray(arr));
+
+// split --> here we are using to seperate the sentence and then converting to Array
+
+let sntc = "Hi Harsh How are you? and what are you doing now?";
+
+console.log(sntc);
+
+arr = sntc.split(' ');
+
+console.log(arr);
+
+// join --> here we are using this method to make sentence from an array
+
+let j1Arr = arr.join();   // if we do not provide any parameter then by default it joins using ','(comma)
+
+console.log(j1Arr);   // Hi,Harsh,How,are,you?,and,what,are,you,doing,now?
+
+j1Arr = arr.join(' '); 
+
+console.log(j1Arr);     // Hi Harsh How are you? and what are you doing now?
+
+// use of + and concat 
+
+let arr1 = ["a","b","c","d"];
+
+let arr2 = ["e","f","g"];
+
+let arr3 = arr1+arr2;       // result datatype --> String
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+arr3 = arr1.concat(arr2);   // result datatype --> Array
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+books = ["Maths","Science","Physics","Chemistry"];
+
+let booksWithPages = [
+    ["Math","100"],
+    ["Science","200"],
+
+];
+
+console.log(booksWithPages);
+
+console.log("booksWithPages[0][1]",booksWithPages[0][1]);
+
+booksWithPages = [
+    ["Math",["100","200"]],
+    ["Science","200"],
+
+];
+
+console.log(booksWithPages);
+
+console.log("booksWithPages[0][1]",booksWithPages[0][1]);
+
+console.log("booksWithPages[0][1][1]",booksWithPages[0][1][1]);
+
+console.log("books",books);
+
+for(let i =0;i<books.length;i++){
+    console.log(books[i]);
+
+}
+
+// Getting  Array Elements using ForEach
+books.forEach(myFun);
+
+function myFun(value){
+    console.log(value);
+}
