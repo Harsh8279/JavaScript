362009ba5861b6a4ad657b196ce50f3ae42348d7
Harsh8279 <hhenu8279@gmail.com>
2022-01-21 13:44:15 +0530

    Anonymus function

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -1,25 +1,29 @@
-// Global variable vs Local Variable
+// Anonymus Function
 
-let car = "Lamborghini";    // Global variable
+/*
 
-function doPrint(){
-
-    let car = "Audi";
-    
-    console.log("car is : ",car);
-
-    let a = 25;             // Local variable
-
-    console.log("A is ",a);
-
-    // let car = "BMW";        // ReferenceError: Cannot access 'car' before initialization
-
-    // that means if we want to make our local variable with same name as global variable then 
-    // we have to declare or define at the start of the function 
-
-    console.log("car is ",car);
+function (){        // SyntaxError: Function statements require a function name
+    console.log("Hiiiii");
 }
 
-doPrint();
+*/
 
-console.log("car is ",car);
+let fn = function(){
+    console.log("Hiii I am variable function");
+};
+
+console.log(fn);
+
+fn();
+
+setTimeout(fn,3000);    // it calls function after 3sec 
+
+console.log("Hiiiii");
+
+setTimeout(
+    function (){
+        console.log("Hi I have no name and still I am function so I am Anonymus function");
+    },3000
+);
+
+// Note : Javascript not run line by line
