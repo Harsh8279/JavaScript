332c491778a1cab516b0cd10e76f648b415e016a
Harsh8279 <hhenu8279@gmail.com>
2022-01-30 01:23:10 +0530

    Prototype in JavaScript

--- JSTut/js/script.js
+++ JSTut/js/script.js
@@ -55,3 +55,38 @@ s1.studentDetails = function(){
 console.log(s1.studentDetails());
 
 // If we want common properties then we can also add in our constructor function
+
+// now our requirement is we want to add another common property but not in constructor function additionalyy
+
+Student.prototype.nationality = "Indian";
+
+console.log("Student 1",s1);    
+
+/*
+Student 1 
+    Student {name: 'Harsh', age: 22, cls: 4, dept: 'Computer', studentDetails: ƒ}
+    age: 22
+    cls: 4
+    dept: "Computer"
+    name: "Harsh"
+    studentDetails: ƒ ()
+    [[Prototype]]: Object
+        nationality: "Indian"
+*/
+// If we see output then we get that property inside Prototype object 
+// but we can access it directly like others
+console.log("s1.nationality",s1.nationality)
+console.log("Student 2",s2);
+
+// In same way we can add function also
+
+Student.prototype.getDetails = function(){
+    return "Name "+this.name+" Age is "+this.age+" class is "+this.cls+" Nationality is "+this.nationality;
+};
+
+console.log("Student 1",s1);
+console.log("Student 2",s2);
+
+console.log(s1.getDetails());
+
+console.log(s2.getDetails());
